5346c0642cde7f80d7e8c480ef01aa975cc6b19d
Matti Laamanen <mattilaa@gmail.com>
2024-07-11 20:01:52 +0300

    Add clang-format generation support, fixed strings

--- cmake_project_creator.py
+++ cmake_project_creator.py
@@ -3,7 +3,15 @@
 import argparse
 import os
 
-def create_cmake_project(project_path, project_name, cpp_standard, is_lib, add_benchmark, enable_asan):
+
+def create_cmake_project(
+        project_path,
+        project_name,
+        cpp_standard,
+        is_lib,
+        add_benchmark,
+        enable_asan,
+        create_format):
     # Expand the user home directory symbol (~) to the full path
     project_path = os.path.expanduser(project_path)
     project_dir = os.path.join(project_path)
@@ -23,8 +31,7 @@ def create_cmake_project(project_path, project_name, cpp_standard, is_lib, add_b
         os.makedirs(benchmarks_dir, exist_ok=True)
 
     # Create example header in include directory
-    hello_h_content = """
-#ifndef {upper_project_name}_H
+    hello_h_content = """#ifndef {upper_project_name}_H
 #define {upper_project_name}_H
 
 #include <iostream>
@@ -35,7 +42,7 @@ namespace example
 }}
 
 #endif
-    """.format(upper_project_name=upper_project_name)
+""".format(upper_project_name=upper_project_name)
 
     with open(os.path.join(include_dir, project_name + ".h"), "w") as file:
         file.write(hello_h_content)
@@ -50,24 +57,22 @@ namespace example
         return a + b;
     }}
 }}
-        """.format(project_name=project_name)
+"""
         with open(os.path.join(src_dir, project_name + ".cpp"), "w") as file:
             file.write(mylib_cpp_content)
 
     else:
         # Create main.cpp in src directory
-        main_cpp_content = """
-#include "{project_name}.h"
+        main_cpp_content = """#include "{project_name}.h"
 
 int main(int argc, char* argv [])
 {{
     std::cout << "Hello result: " << example::add(2, 4) << "\\n";
 }}
-        """.format(project_name=project_name)
+""".format(project_name=project_name)
         with open(os.path.join(src_dir, "main.cpp"), "w") as file:
             file.write(main_cpp_content)
-        example_cpp_content = """
-#include "{project_name}.h"
+        example_cpp_content = """#include "{project_name}.h"
 
 namespace example
 {{
@@ -76,13 +81,12 @@ namespace example
         return a * b;
     }}
 }}
-        """.format(project_name=project_name)
+""".format(project_name=project_name)
         with open(os.path.join(src_dir, project_name + ".cpp"), "w") as file:
             file.write(example_cpp_content)
 
     # Create test.cpp in tests directory
-    test_cpp_content = """
-#include <gtest/gtest.h>
+    test_cpp_content = """#include <gtest/gtest.h>
 #include "{project_name}.h"
 
 TEST(HelloTest, BasicAssertions)
@@ -98,13 +102,12 @@ int main(int argc, char **argv)
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }}
-    """.format(project_name=project_name)
+""".format(project_name=project_name)
     with open(os.path.join(tests_dir, "test.cpp"), "w") as file:
         file.write(test_cpp_content)
 
     if add_benchmark:
-        benchmark_cpp_content = """
-#include <benchmark/benchmark.h>
+        benchmark_cpp_content = """#include <benchmark/benchmark.h>
 #include <vector>
 #include <algorithm>
 
@@ -137,13 +140,12 @@ static void BM_ExampleFunction(benchmark::State& state)
 BENCHMARK(BM_ExampleFunction)->Range(8, 8<<10);
 
 BENCHMARK_MAIN();
-        """
+"""
         with open(os.path.join(benchmarks_dir, "benchmark.cpp"), "w") as file:
             file.write(benchmark_cpp_content)
 
     # Create CMakeLists.txt in the root directory
-    cmake_lists_content = f"""
-cmake_minimum_required(VERSION 3.10)
+    cmake_lists_content = f"""cmake_minimum_required(VERSION 3.12)
 project({project_name})
 
 # Create compile_commands.json in build directory
@@ -155,18 +157,17 @@ set(CMAKE_CXX_STANDARD_REQUIRED True)
 
 # Add include directory
 include_directories(include)
-
-    """
+"""
     if is_lib:
         cmake_lists_content = cmake_lists_content + """
 # Library target
 add_library({project_name} src/{project_name}.cpp)
-        """.format(project_name=project_name)
+""".format(project_name=project_name)
     else:
         cmake_lists_content = cmake_lists_content + """
 # Executable target
 add_executable({project_name} src/main.cpp include/{project_name}.h src/{project_name}.cpp)
-        """.format(project_name=project_name)
+""".format(project_name=project_name)
 
     cmake_lists_content = cmake_lists_content + """
 # Fetch GoogleTest
@@ -194,7 +195,7 @@ target_link_libraries(
 
 include(GoogleTest)
 gtest_discover_tests(test_{project_name})
-    """.format(project_name=project_name)
+""".format(project_name=project_name)
 
     if enable_asan:
         cmake_lists_content = cmake_lists_content + """
@@ -206,7 +207,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ASAN_FLAGS}")
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${ASAN_FLAGS}")
 endif()
-    """
+"""
 
     if add_benchmark:
         cmake_lists_content = cmake_lists_content + """
@@ -227,36 +228,97 @@ add_executable(
 target_link_libraries(
   benchmark_{project_name} benchmark::benchmark
 )
-    """.format(project_name=project_name)
+""".format(project_name=project_name)
+    cmake_lists_content = cmake_lists_content + "\n"
 
     with open(os.path.join(project_dir, "CMakeLists.txt"), "w") as file:
         file.write(cmake_lists_content)
 
+    if create_format:
+        clang_format_contents = """Language:        Cpp
+BasedOnStyle:    Google
+IndentWidth:     4
+TabWidth:        4
+UseTab:          Never
+BreakBeforeBraces: Allman
+AllowShortIfStatementsOnASingleLine: false
+ColumnLimit:     80
+PointerAlignment: Left
+SpaceAfterControlStatementKeyword: true
+IndentPPDirectives: AfterHash
+ConstructorInitializerIndentWidth: 2
+ContinuationIndentWidth: 2
+SpaceBeforeParens: Custom
+SpaceBeforeParensOptions:
+  AfterControlStatements: false
+"""
+
+        with open(os.path.join(project_dir, ".clang-format"), "w") as file:
+            file.write(clang_format_contents)
+
     # Create README.md in the root directory
-    readme_content = """
-Empty project
-    """
+    readme_content = """Empty project"""
     with open(os.path.join(project_dir, "README.rd"), "w") as file:
         file.write(readme_content)
 
     print(f"CMake project '{project_name}' created successfully in '{project_path}'!")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Create a simple CMake project with GoogleTest setup.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('project_name', type=str, help='The name of the project')
-    parser.add_argument('--std', type=str, choices=['c++11', 'c++14', 'c++17', 'c++20'], default='c++11', help='The C++ standard version (c++11, c++14, c++17, c++20)')
-    parser.add_argument('--lib', dest='lib', action='store_true', help="Create library project instead of program project")
-    parser.add_argument('--path', type=str, default='.', help='The root path where the project will be created')
-    parser.add_argument('--add-benchmark', dest='benchmark', action='store_true', help="Add Google Benchmark into the project")
-    parser.add_argument('--enable-asan', dest='asan', action='store_true', help="Enable address sanitizer")
+    parser.add_argument("project_name", type=str, help="The name of the project")
+    parser.add_argument(
+        "--std",
+        type=str,
+        choices=["c++11", "c++14", "c++17", "c++20"],
+        help="The C++ standard version (c++11, c++14, c++17, c++20)",
+    )
+    parser.add_argument(
+        "--lib",
+        dest="lib",
+        action="store_true",
+        help="Create library project instead of program project",
+    )
+    parser.add_argument(
+        "--path",
+        type=str,
+        default=".",
+        help="The root path where the project will be created",
+    )
+    parser.add_argument(
+        "--add-benchmark",
+        dest="benchmark",
+        action="store_true",
+        help="Add Google Benchmark into the project",
+    )
+    parser.add_argument(
+        "--enable-asan",
+        dest="asan",
+        action="store_true",
+        help="Enable address sanitizer",
+    )
+    parser.add_argument(
+        "--create-formatfile",
+        dest="create_format",
+        action="store_true",
+        help="Create .clang-format file",
+    )
 
     args = parser.parse_args()
 
-    # Extract the C++ standard version number from the input string (e.g., "c++17" -> "17")
+    # Extract the C++ standard version number from the input string
+    # (e.g., "c++17" -> "17")
     cpp_standard = args.std.split('++')[-1]
 
-    create_cmake_project(args.path, args.project_name, cpp_standard, args.lib, args.benchmark, args.asan)
-
+    create_cmake_project(
+        args.path,
+        args.project_name,
+        cpp_standard,
+        args.lib,
+        args.benchmark,
+        args.asan,
+        args.create_format,
+    )
